b7a5fdfd4dc834c7c32ff700051482f5d4826a0e
x-sheep <x-sheep@users.noreply.github.com>
2017-04-27 15:01:59 +0200

    Support new upstream function for hierarchial preset menus
    Per upstream commit a7dc17c4258837b0ee3927f1db5e1c02acee5cc3

--- seismic.c
+++ seismic.c
@@ -1869,7 +1869,7 @@ static void game_print(drawing *dr, const game_state *state, int tilesize)
 const struct game thegame = {
 	"Seismic", NULL, NULL,
 	default_params,
-	game_fetch_preset,
+	game_fetch_preset, NULL,
 	decode_params,
 	encode_params,
 	free_params,
